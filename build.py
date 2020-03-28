ce273e8027b90fd00ec9ca84be62c69edd7de319
vsri1137 <thenewvivek@gmail.com>
2020-03-28 08:34:16 -0700

    Phase 1: main function

--- build.py
+++ build.py
@@ -1,13 +1,16 @@
-top = open("templates/top.html").read() 
-bottom = open("templates/bottom.html").read()
-content = [            
-    "index.html",
-    "about.html",
-    "blog.html",
-    "projects.html",
-]
+def main():
+    top = open("templates/top.html").read() 
+    bottom = open("templates/bottom.html").read()
+    content = [            
+        "index.html",
+        "about.html",
+        "blog.html",
+        "projects.html",
+    ]
 
-for page in content:
-    middle_content = open("content/"+page).read() #opens each html in list, stores content for middle
-    combined_html = top + middle_content + bottom
-    open("docs/"+page,"w+").write(combined_html) #writes concatenated content to new file
+    for page in content:
+        middle_content = open("content/"+page).read() #opens each html in list, stores content for middle
+        combined_html = top + middle_content + bottom
+        open("docs/"+page,"w+").write(combined_html) #writes concatenated content to new file
+
+main()
