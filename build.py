e4b88e77e34402fb1fc7ced7d9e3dbf8004cfd79
vsri1137 <thenewvivek@gmail.com>
2020-03-28 13:51:52 -0700

    Phase 2: Loop

--- build.py
+++ build.py
@@ -1,37 +1,33 @@
 def main():
     top = open("templates/top.html").read() 
     bottom = open("templates/bottom.html").read()
+    pages = [
+        {
+            "filename" : "content/index.html",
+            "output" : "docs/index.html",
+            "title" : "Home",
+        } ,  
+        {
+            "filename" : "content/about.html",
+            "output" : "docs/about.html",
+            "title" : "About me",
+        },
+        {
+            "filename" : "content/blog.html",
+            "output" : "docs/blog.html",
+            "title" : "Blog",
+        },
+        {
+            "filename" : "content/projects.html",
+            "output" : "docs/projects.html",
+            "title" : "Projects",
+        },
+
+    ]
 
     for page in pages:
-        print(pages)
-        middle_content = open(pages[0]["filename"]).read() #read from pages list
+        middle_content = open(page["filename"]).read() #read from pages list
         combined_html = top + middle_content + bottom
-        fred = open("/docs/index.html").write(combined_html) #writes concatenated content to new file
-        print(fred)
-
-
-pages = [
-    {
-        "filename" : "content/index.html",
-        "output" : "docs/index.html",
-        "title" : "Home",
-    } ,  
-    {
-        "filename" : "content/about.html",
-        "output" : "docs/about.html",
-        "title" : "About me",
-    },
-    {
-        "filename" : "content/blog.html",
-        "output" : "docs/blog.html",
-        "title" : "Blog",
-    },
-    {
-        "filename" : "content/projects.html",
-        "output" : "docs/projects.html",
-        "title" : "Projects",
-    },
-
-]
+        open(page["output"], "w+").write(combined_html) #writes concatenated content to new file
 
 main()
