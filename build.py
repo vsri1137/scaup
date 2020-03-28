832bbb1f5e4fbbb28818cf385691150b6b2e9cd9
vsri1137 <thenewvivek@gmail.com>
2020-03-28 08:37:57 -0700

    Phase 2 - added list

--- build.py
+++ build.py
@@ -13,4 +13,32 @@ def main():
         combined_html = top + middle_content + bottom
         open("docs/"+page,"w+").write(combined_html) #writes concatenated content to new file
 
+
+pages = [
+    {
+        "filename" : "content/index.html",
+        "output" : "docs/index.html",
+        "title" : "Home",
+    }   
+    {
+        "filename" : "content/about.html",
+        "output" : "docs/about.html",
+        "title" : "About me",
+    }
+    {
+        "filename" : "content/blog.html",
+        "output" : "docs/blog.html",
+        "title" : "Blog",
+    }
+    {
+        "filename" : "content/projects.html",
+        "output" : "docs/projects.html",
+        "title" : "Projects",
+    }
+
+]
+
+
+
+
 main()
