d193ff07fb263bb7daaff90d28aee34cc2f52a0b
vsri1137 <thenewvivek@gmail.com>
2020-03-28 09:41:47 -0700

    Added list to build.py

--- build.py
+++ build.py
@@ -1,17 +1,13 @@
 def main():
     top = open("templates/top.html").read() 
     bottom = open("templates/bottom.html").read()
-    content = [            
-        "index.html",
-        "about.html",
-        "blog.html",
-        "projects.html",
-    ]
 
-    for page in content:
-        middle_content = open("content/"+page).read() #opens each html in list, stores content for middle
+    for page in pages:
+        print(pages)
+        middle_content = open(pages[0]["filename"]).read() #read from pages list
         combined_html = top + middle_content + bottom
-        open("docs/"+page,"w+").write(combined_html) #writes concatenated content to new file
+        fred = open("/docs/index.html").write(combined_html) #writes concatenated content to new file
+        print(fred)
 
 
 pages = [
@@ -19,26 +15,23 @@ pages = [
         "filename" : "content/index.html",
         "output" : "docs/index.html",
         "title" : "Home",
-    }   
+    } ,  
     {
         "filename" : "content/about.html",
         "output" : "docs/about.html",
         "title" : "About me",
-    }
+    },
     {
         "filename" : "content/blog.html",
         "output" : "docs/blog.html",
         "title" : "Blog",
-    }
+    },
     {
         "filename" : "content/projects.html",
         "output" : "docs/projects.html",
         "title" : "Projects",
-    }
+    },
 
 ]
-
-
-
 
 main()
