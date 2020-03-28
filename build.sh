137f2cedb1f0d839abbd48cd6c01c1a769fe0408
vsri1137 <thenewvivek@gmail.com>
2020-03-27 22:06:06 -0700

    Updated bash script

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-cat templates/top.html content/index.html templates/bottom.html > docs/index_combined.html
+#!/bin/bash
+
+
+#cat templates/top.html content/index.html templates/bottom.html > docs/index_combined.html
+
+dir= /Users/vsri/Library/Mobile\ Documents/com~apple~CloudDocs/code/solo/content/*
+
+for file in $dir
+do
+    echo $file
+done
