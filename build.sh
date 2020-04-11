903fbe1928a2499ef378d2cdad000ad22f849435
vsri1137 <thenewvivek@gmail.com>
2020-04-10 18:01:07 -0700

    delete useless files

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
-cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
-cat templates/top.html content/about.html templates/bottom.html > docs/about.html
